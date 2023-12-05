7fe27b58d4f26acde3ecc54ba328107d44104c3d
Olga <minaeva9@gmail.com>
2023-12-05 18:06:34 -0500

    reset function added

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const countEl = document.getElementById("count");
 const incrementBtn = document.getElementById("increment-btn");
 const saveBtn = document.getElementById("save-btn");
 const entriesEl = document.getElementById("entries");
+const resetBtn = document.getElementById("reset-btn");
 
 let count = 0;
 
 incrementBtn.addEventListener("click", increment);
 saveBtn.addEventListener("click", save);
+resetBtn.addEventListener("click", clearData);
 
 function increment() {
   count++;
@@ -22,4 +24,10 @@ function save() {
 function reset() {
   count = 0;
   countEl.textContent = 0;
+}
+
+function clearData() {
+  count = 0;
+  countEl.textContent = 0;
+  entriesEl.textContent = "Previous entries: ";
 }
