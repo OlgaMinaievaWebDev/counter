068a87abca755d4a16e68c3533658a8cddb0e3d9
Olga <minaeva9@gmail.com>
2023-12-04 23:03:54 -0500

    save function added

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const countEl = document.getElementById("count");
 const incrementBtn = document.getElementById("increment-btn");
+const saveBtn = document.getElementById("save-btn");
 
 let count = 0;
 
 incrementBtn.addEventListener("click", increment);
+saveBtn.addEventListener('click', save);
 
 function increment() {
   count++;
   countEl.textContent = count;
+}
+
+function save() {
+ console.log(count)
 }
